540ffeec73f13425ee7a0ada21a926ae49a02b25
Arsha27 <110929229+Arsha27@users.noreply.github.com>
2023-09-17 14:57:26 +0530

    init

---
+++ app/src/main/java/com/example/moviesapp/MainActivity.java
@@ -0,0 +1,21 @@
+package com.example.moviesapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+    RecyclerView videoList;
+    VideoAdapter adapter;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        videoList = findViewById(R.id.videoList);
+        videoList.setLayoutManager(new LinearLayoutManager(this));
+        adapter = new VideoAdapter();
+        videoList.setAdapter(adapter);
+    }
+}
